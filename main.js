61ccd3242a1f56b3c7180e61b7b16fcc698fa30c
orozbek6622 <sabaevoroz@gmail.com>
2023-08-04 14:41:40 +0300

    zoro

---
+++ main.js
@@ -0,0 +1,16 @@
+const greeting= (param)=>{
+    alert( param)
+}
+
+const btn=document.querySelector('button')
+GamepadButton.onclik
+let sas = 12
+const h1 = document.querySelector('h1')
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+button.onclick =()=>{
+
+
+h1.innerHTML= input.value
+    
+}
